0d115f2f6971e2fb8ce36ffc34595cd34a915c02
Muhammad Hoti <muhammad.hoti@gmail.com>
2018-10-08 00:34:15 +0500

    Updates

---
+++ precache-manifest.bb8e279b6b65f7512a5e1819698e83e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eaa51f4288cccd523939",
+    "url": "/create_fb_post/static/js/runtime~main.eaa51f42.js"
+  },
+  {
+    "revision": "c4a54247de72aa15883c",
+    "url": "/create_fb_post/static/js/main.c4a54247.chunk.js"
+  },
+  {
+    "revision": "7c460c7ed4d21bd47ca5",
+    "url": "/create_fb_post/static/js/1.7c460c7e.chunk.js"
+  },
+  {
+    "revision": "c4a54247de72aa15883c",
+    "url": "/create_fb_post/static/css/main.aa24ebf7.chunk.css"
+  },
+  {
+    "revision": "7c460c7ed4d21bd47ca5",
+    "url": "/create_fb_post/static/css/1.648cff3c.chunk.css"
+  },
+  {
+    "revision": "143aead4aa7548af94860142443d29d9",
+    "url": "/create_fb_post/index.html"
+  }
+];
